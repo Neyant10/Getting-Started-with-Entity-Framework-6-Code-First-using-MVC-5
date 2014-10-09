c735c3dfc887c8a056628d0e921578fff6b202fc
Stian G. Skjerveggen <stian.skjerveggen@gmail.com>
2014-10-09 15:22:55 +0200

    viewmodel for enrollment stats

---
+++ ContosoUniversity/ViewModels/EnrollmentDateGroup.vb
@@ -0,0 +1,12 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+Namespace ViewModels
+    Public Class EnrollmentDateGroup
+
+        <DataType(DataType.Date)>
+        Public Property EnrollmentDate As DateTime?
+
+        Public Property StudentCount As Integer
+    End Class
+
+End Namespace
