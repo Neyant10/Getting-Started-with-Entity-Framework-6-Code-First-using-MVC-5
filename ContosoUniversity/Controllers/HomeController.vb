5db205c0da9e7af842a96be7fee78e868998f02e
Stian G. Skjerveggen <stian.skjerveggen@gmail.com>
2014-10-09 15:21:55 +0200

    Added statistics

--- ContosoUniversity/Controllers/HomeController.vb
+++ ContosoUniversity/Controllers/HomeController.vb
@@ -1,14 +1,21 @@
-﻿Public Class HomeController
+﻿Imports ContosoUniversity.DAL
+Imports ContosoUniversity.ViewModels
+
+Public Class HomeController
     Inherits System.Web.Mvc.Controller
+
+    Private db As SchoolContext = New SchoolContext
 
     Function Index() As ActionResult
         Return View()
     End Function
 
     Function About() As ActionResult
-        ViewData("Message") = "Your application description page."
+        Dim data As IQueryable(Of EnrollmentDateGroup) = From student In db.Students
+                                                         Group student By student.EnrollmentDate Into Count()
+                                                         Select New EnrollmentDateGroup With {.EnrollmentDate = EnrollmentDate, .StudentCount = Count}
 
-        Return View()
+        Return View(data.ToList)
     End Function
 
     Function Contact() As ActionResult
@@ -16,4 +23,9 @@
 
         Return View()
     End Function
+
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        db.Dispose()
+        MyBase.Dispose(disposing)
+    End Sub
 End Class
