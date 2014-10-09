4d525b2b8fd7715d6e1d0ddc8b4280b817553b15
Stian G. Skjerveggen <stian.skjerveggen@gmail.com>
2014-10-09 13:28:29 +0200

    added annotations

--- ContosoUniversity/Models/Student.vb
+++ ContosoUniversity/Models/Student.vb
@@ -1,9 +1,21 @@
-﻿Namespace Models
+﻿Imports System.ComponentModel
+Imports System.ComponentModel.DataAnnotations
+
+Namespace Models
     Public Class Student
         Public Property ID As Integer
+
+        <DisplayName("Last Name")>
         Public Property LastName As String
+
+        <DisplayName("First Name")>
         Public Property FirstMidName As String
+
+        <DisplayName("Enrollment Date")>
+        <DataType(DataType.Date)>
+        <DisplayFormat(DataFormatString:="{0:d}", ApplyFormatInEditMode:=True)>
         Public Property EnrollmentDate As DateTime
+
         Public Overridable Property Enrollments As ICollection(Of Enrollment)
 
     End Class
