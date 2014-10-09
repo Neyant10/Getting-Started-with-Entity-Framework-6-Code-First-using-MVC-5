66379d2f61793a481c85c97559fba8e8008516d5
Stian G. Skjerveggen <stian.skjerveggen@gmail.com>
2014-10-09 10:44:53 +0200

    added context and seed

--- ContosoUniversity/DAL/SchoolContext.vb
+++ ContosoUniversity/DAL/SchoolContext.vb
@@ -13,7 +13,6 @@ Namespace DAL
         Public Property Students As DbSet(Of Student)
         Public Property Enrollments As DbSet(Of Enrollment)
         Public Property Courses As DbSet(Of Course)
-
         Protected Overrides Sub OnModelCreating(modelBuilder As DbModelBuilder)
             modelBuilder.Conventions.Remove(Of PluralizingTableNameConvention)()
         End Sub
