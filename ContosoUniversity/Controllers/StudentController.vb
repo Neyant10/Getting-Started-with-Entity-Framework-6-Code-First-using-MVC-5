f2c41fa63f239a895fd00fd2ab57165496fad2c3
Stian G. Skjerveggen <stian.skjerveggen@gmail.com>
2014-10-09 13:29:20 +0200

    added sort order

--- ContosoUniversity/Controllers/StudentController.vb
+++ ContosoUniversity/Controllers/StudentController.vb
@@ -16,8 +16,24 @@ Namespace Controllers
         Private db As New SchoolContext
 
         ' GET: Student
-        Function Index() As ActionResult
-            Return View(db.Students.ToList())
+        Function Index(sortOrder As String) As ActionResult
+            ViewBag.NameSortParam = If(String.IsNullOrEmpty(sortOrder), "name_desc", "")
+            ViewBag.DateSortParam = If(sortOrder = "Date", "date_desc", "Date")
+
+            Dim students = From s In db.Students
+                           Select s
+
+            Select Case sortOrder
+                Case "name_desc"
+                    students = students.OrderByDescending(Function(s) s.LastName)
+                Case "Date"
+                    students = students.OrderBy(Function(s) s.EnrollmentDate)
+                Case "date_desc"
+                    students = students.OrderByDescending(Function(s) s.EnrollmentDate)
+                Case Else
+                    students = students.OrderBy(Function(s) s.LastName)
+            End Select
+            Return View(students.ToList())
         End Function
 
         ' GET: Student/Details/5
@@ -42,12 +58,18 @@ Namespace Controllers
         'more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         <HttpPost()>
         <ValidateAntiForgeryToken()>
-        Function Create(<Bind(Include:="ID,LastName,FirstMidName,EnrollmentDate")> ByVal student As Student) As ActionResult
-            If ModelState.IsValid Then
-                db.Students.Add(student)
-                db.SaveChanges()
-                Return RedirectToAction("Index")
-            End If
+        Function Create(<Bind(Include:="LastName,FirstMidName,EnrollmentDate")> ByVal student As Student) As ActionResult
+            Try
+                If ModelState.IsValid Then
+                    db.Students.Add(student)
+                    db.SaveChanges()
+                    Return RedirectToAction("Index")
+                End If
+            Catch dex As DataException
+                Debug.WriteLine(dex.ToString)
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your network administrator.")
+            End Try
+
             Return View(student)
         End Function
 
@@ -69,18 +91,26 @@ Namespace Controllers
         <HttpPost()>
         <ValidateAntiForgeryToken()>
         Function Edit(<Bind(Include:="ID,LastName,FirstMidName,EnrollmentDate")> ByVal student As Student) As ActionResult
-            If ModelState.IsValid Then
-                db.Entry(student).State = EntityState.Modified
-                db.SaveChanges()
-                Return RedirectToAction("Index")
-            End If
+            Try
+                If ModelState.IsValid Then
+                    db.Entry(student).State = EntityState.Modified
+                    db.SaveChanges()
+                    Return RedirectToAction("Index")
+                End If
+            Catch dex As DataException
+                Debug.WriteLine(dex.ToString)
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.")
+            End Try
             Return View(student)
         End Function
 
         ' GET: Student/Delete/5
-        Function Delete(ByVal id As Integer?) As ActionResult
+        Function Delete(ByVal id As Integer?, Optional saveChangesError As Boolean? = False) As ActionResult
             If IsNothing(id) Then
                 Return New HttpStatusCodeResult(HttpStatusCode.BadRequest)
+            End If
+            If saveChangesError.GetValueOrDefault Then
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator."
             End If
             Dim student As Student = db.Students.Find(id)
             If IsNothing(student) Then
@@ -91,12 +121,15 @@ Namespace Controllers
 
         ' POST: Student/Delete/5
         <HttpPost()>
-        <ActionName("Delete")>
         <ValidateAntiForgeryToken()>
-        Function DeleteConfirmed(ByVal id As Integer) As ActionResult
-            Dim student As Student = db.Students.Find(id)
-            db.Students.Remove(student)
-            db.SaveChanges()
+        Function Delete(ByVal id As Integer) As ActionResult
+            Try
+                Dim student As Student = db.Students.Find(id)
+                db.Students.Remove(student)
+                db.SaveChanges()
+            Catch dex As DataException
+                Return RedirectToAction("Delete", New With {.id = id, .saveChangesError = True})
+            End Try
             Return RedirectToAction("Index")
         End Function
 
