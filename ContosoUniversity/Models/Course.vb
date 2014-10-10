c41ad7f69e59f1c0a6f2c597e9fe8dd547246e31
Stian G. Skjerveggen <stian.skjerveggen@gmail.com>
2014-10-10 11:03:26 +0200

    added fields and annotations

--- ContosoUniversity/Models/Course.vb
+++ ContosoUniversity/Models/Course.vb
@@ -1,12 +1,27 @@
-﻿Imports System.ComponentModel.DataAnnotations.Schema
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
 
 Namespace Models
     Public Class Course
         <DatabaseGenerated(DatabaseGeneratedOption.None)>
+        <Display(Name:="Number")>
         Public Property CourseID As Integer
+
+        <StringLength(50, MinimumLength:=3)>
         Public Property Title As String
+
+
+        <Range(0, 5)>
         Public Property Credits As String
+
+        Public Property DepartmentID As Integer
+
+        Public Overridable Property Department As Department
+
         Public Overridable Property Enrollments As ICollection(Of Enrollment)
+
+        Public Overridable Property Instructors As ICollection(Of Instructor)
+
     End Class
 
 End Namespace
