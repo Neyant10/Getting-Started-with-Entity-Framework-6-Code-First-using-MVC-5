aff16d1f5fb26f5ed36a508e50174af55251c75c
Stian G. Skjerveggen <stian.skjerveggen@gmail.com>
2014-10-10 10:49:54 +0200

    Added annotations

--- ContosoUniversity/Models/Student.vb
+++ ContosoUniversity/Models/Student.vb
@@ -1,23 +1,32 @@
 ﻿Imports System.ComponentModel
 Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
 
 Namespace Models
     Public Class Student
         Public Property ID As Integer
 
-        <DisplayName("Last Name")>
+        <Display(Name:="Last Name")>
         <StringLength(50)>
+        <Required>
         Public Property LastName As String
 
-        <DisplayName("First Name")>
+        <Display(Name:="First Name")>
+        <Column("FirstName")>
         <StringLength(50, ErrorMessage:="First name cannot be longer than 50 characters")>
         Public Property FirstMidName As String
 
-        <DisplayName("Enrollment Date")>
+        <Display(Name:="Enrollment Date")>
         <DataType(DataType.Date)>
         <DisplayFormat(DataFormatString:="{0:d}", ApplyFormatInEditMode:=True)>
         Public Property EnrollmentDate As DateTime
 
+        <Display(Name:="Full Name")>
+        Public ReadOnly Property FullName
+            Get
+                Return LastName + ", " + FirstMidName
+            End Get
+        End Property
         Public Overridable Property Enrollments As ICollection(Of Enrollment)
 
     End Class
